04343e41346c00f98f2542476fa39b354b14c9bb
KuliakQA <culeacvadim@gmail.com>
2022-01-18 20:08:02 +0100

    added test

---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.experimental.UtilityClass;
+
+import java.util.Locale;
+
+@UtilityClass
+public class DataGenerator {
+
+    @UtilityClass
+    public static class Form {
+        public static FormInfo generateInfo(String locale) {
+            Faker faker = new Faker(new Locale(locale));
+            return new FormInfo(faker.address().cityName(),
+                    faker.name().lastName() + " " + faker.name().firstName(),
+                    faker.numerify("+7##########"));
+        }
+
+    }
+
+}
+
+
---
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -0,0 +1,60 @@
+package ru.netology;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class DeliveryCardTest {
+
+    @BeforeEach
+    void setUpAll() {
+        open("http://localhost:9999");
+    }
+
+    public String dateFormat(int days) {
+        return LocalDate.now().plusDays(days).
+                format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    public void shouldSuccessfulSendValidForm() {
+        Configuration.holdBrowserOpen = true;
+        FormInfo info = DataGenerator.Form.generateInfo("ru");
+        $("[data-test-id='city'] input").setValue(info.getCity());
+        String date = dateFormat(8);
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Успешно!"),
+                        Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + date));
+        String dateChange = dateFormat(17);
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(dateChange);
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification']").
+                shouldHave(Condition.text("Необходимо подтверждение"),
+                        Duration.ofSeconds(15));
+        $(withText("Перепланировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + dateChange));
+
+    }
+}
